bb54c2c8931cac6d66fbd923fce6d070f144672f
Rohit Kumar Suman <rohitkumarsuman2011india@gmail.com>
2023-01-29 18:07:44 +0530

    site complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { useEffect } from "react";
 import TypeWriterEffect from "react-typewriter-effect";
-import Tilt from "react-parallax-tilt";
 
 import { linksName, styles } from "./constants";
 import Switcher from "./components/Switcher";
 import BackgroundBlob from "./components/background/BackgroundBlob";
 import rohitpfp from "./assets/rohitpfp.png";
 import "./normal.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
-    <div className=" min-h-screen bg-[#f1f1f1] dark:bg-main-dark-bg transition-all duration-300 text-black dark:text-white ">
-      <div className="relative flex justify-center items-center min-h-screen">
+    <div className={styles.appStyles}>
+      <div className={`relative ${styles.flexCenter} min-h-screen`}>
         <BackgroundBlob />
 
         <div className="absolute top-4 left-4 ">
@@ -18,65 +25,60 @@ export default function App() {
         </div>
         <div className={`min-h-screen flex-col ${styles.flexBetween} z-[2]`}>
           <div className="flex items-center flex-col">
-            <Tilt perspective={500} gyroscope={true} className="transform-gpu ">
-              <div
-                className={`${styles.flexCenter} flex-col w-fit p-8 rounded-[50px] mb-16 mt-[3rem] lg:mt-8 lg:mb-8 2xl:mt-20 2xl:mb-20 ${styles.glassEffect}`}
-              >
-                <div className="mb-4 2xl:mb-8 ">
-                  <img
-                    src={rohitpfp}
-                    alt="rohit-pfp"
-                    className="w-32 h-32 md:w-40 md:h-40 lg:w-32 lg:h-32 2xl:w-40 2xl:h-40 object-fill rounded-xl"
-                  />
-                </div>
-                <div className="text-center">
-                  <h1 className="text-xl md:text-2xl lg:text-16 2xl:text-2xl font-rubik mb-[5px] ">
-                    Hi, I'm Rohit a
-                  </h1>
-                  <span className="text-2xl md:text-[32px] lg:text-[26px] 2xl:text-[32px] font-kanit font-bold">
-                    <TypeWriterEffect
-                      // multiTextLoop="true"
-                      startDelay={500}
-                      cursorColor="red"
-                      multiText={[
-                        "Web2 Frontend-Developer",
-                        "Fullstack Web3 Developer",
-                        "Solidity Developer",
-                      ]}
-                      multiTextDelay={1500}
-                      typeSpeed={40}
-                    />
-                  </span>
-                </div>
+            <div
+              data-aos="fade-down"
+              className={`${styles.flexCenter} ${styles.glassEffect} ${styles.heroDivStyles}`}
+            >
+              <div className="mb-4 2xl:mb-8 ">
+                <img
+                  src={rohitpfp}
+                  alt="rohit-pfp"
+                  className={styles.heroImage}
+                />
               </div>
-            </Tilt>
+              <div className="text-center">
+                <h1 className="text-xl md:text-2xl lg:text-16 2xl:text-2xl font-rubik mb-[5px] ">
+                  Hi, I'm Rohit a
+                </h1>
+                <span className="text-2xl md:text-[32px] lg:text-[26px] 2xl:text-[32px] font-kanit font-bold">
+                  <TypeWriterEffect
+                    multiTextLoop="true"
+                    startDelay={300}
+                    cursorColor="red"
+                    multiText={[
+                      "Web2 Frontend-Developer",
+                      "Fullstack Web3 Developer",
+                      "Solidity Developer",
+                    ]}
+                    multiTextDelay={1000}
+                    typeSpeed={40}
+                  />
+                </span>
+              </div>
+            </div>
             <div className="flex flex-col ">
               {linksName.map((link, i) => (
-                <Tilt className="transform-gpu " key={i}>
-                  <div
-                    className={`mb-8 lg:mb-[1.2rem] 2xl:mb-8 py-2 lg:py-[0.3rem] 2xl:py-2 px-20 md:px-[13rem] lg:px-80 rounded-[10px] lg:rounded-[5px] 2xl:rounded-[10px]
-                  ${styles.glassEffect} before:${
-                      link.name === link.name && link.bg
-                    } hoverEffect `}
-                  >
-                    <a className="uppercase ">
-                      <div className="flex items-center gap-4">
-                        <img
-                          src={link.icon}
-                          alt={link.name}
-                          className="w-[40px] h-[40px] md:w-[45px] md:h-[45px] lg:w-[30px] lg:h-[30px] 2xl:w-[45px] 2xl:h-[45px]"
-                        />
-                        <div className="md:text-[22px] lg:text-[14px] 2xl:text-[22px]">
-                          {link.name}
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                </Tilt>
+                <div
+                  key={i}
+                  data-aos={link.direction}
+                  data-aos-delay={link.delay}
+                  className={`hoverEffect ${styles.linksStyles} ${styles.glassEffect} before:bg-gradient-to-r ${link.bg} hover:text-black `}
+                >
+                  <a href="" className="uppercase ">
+                    <div className="flex items-center gap-4">
+                      <img
+                        src={link.icon}
+                        alt={link.name}
+                        className={styles.linkImageWidthHeight}
+                      />
+                      <div className={styles.linkText}>{link.name}</div>
+                    </div>
+                  </a>
+                </div>
               ))}
             </div>
           </div>
-          <div className="mb-8 mt-8 text-center font-rubik md:text-[18px] lg:text-[12px] 2xl:text-[18px]">
+          <div className={styles.copyrightStyles}>
             <p>All right reserved @2021</p>
           </div>
         </div>
--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,7 +1,6 @@
 import github from "../assets/github.svg";
 import linkedin from "../assets/linkedin.svg";
 import portfolio_colored_1 from "../assets/portfolio-colored-1.svg";
-import portfolio_colored_2 from "../assets/portfolio-colored-2.svg";
 import twitter from "../assets/twitter.svg";
 
 const linksName = [
@@ -9,29 +8,57 @@ const linksName = [
     name: "Twitter",
     icon: twitter,
     link: "",
-    bg: "bg-gradient-to-r from-[#ff5f6d] to-[#ffc371]",
+    bg: "from-[#ff5f6d] to-[#ffc371]",
+    delay: "700",
+    direction: "fade-up-right",
   },
   {
     name: "LinkedIn",
     icon: linkedin,
     link: "",
-    bg: "bg-gradient-to-r from-[#36d1dc] to-[#5b86e5]",
+    bg: "from-[#36d1dc] to-[#5b86e5]",
+    delay: "800",
+    direction: "fade-up-left",
   },
   {
     name: "Portfolio",
     icon: portfolio_colored_1,
     link: "",
-    bg: "bg-gradient-to-r from-[#ee9ca7] to-[#ffdde1]",
+    bg: "from-[#ee9ca7] to-[#ffdde1]",
+    delay: "900",
+    direction: "fade-up-right",
   },
   {
     name: "Github",
     icon: github,
     link: "",
-    bg: "bg-gradient-to-r from-[#fdfc47] to-[#24fe41]",
+    bg: "from-[#fdfc47] to-[#24fe41]",
+    delay: "1000",
+    direction: "fade-up-left",
   },
 ];
 
 const styles = {
+  appStyles:
+    "overflow-x-hidden min-h-screen bg-[#f1f1f1] dark:bg-main-dark-bg transition-all duration-300 text-black dark:text-white",
+
+  heroDivStyles:
+    "flex-col w-fit p-8 rounded-[50px] mb-16 mt-[3rem] lg:mt-8 lg:mb-8 2xl:mt-20 2xl:mb-20",
+
+  heroImage:
+    "w-32 h-32 md:w-40 md:h-40 lg:w-32 lg:h-32 2xl:w-40 2xl:h-40 object-fill rounded-xl",
+
+  linksStyles:
+    "py-2 lg:py-[0.3rem] 2xl:py-2 px-20 md:px-[13rem] lg:px-80 mb-8 lg:mb-[1.2rem] 2xl:mb-8 rounded-[10px] lg:rounded-[5px] 2xl:rounded-[10px] cursor-pointer",
+
+  linkImageWidthHeight:
+    "w-[40px] h-[40px] md:w-[45px] md:h-[45px] lg:w-[30px] lg:h-[30px] 2xl:w-[45px] 2xl:h-[45px]",
+
+  linkText: "md:text-[22px] lg:text-[14px] 2xl:text-[22px]",
+
+  copyrightStyles:
+    "mb-8 mt-8 text-center font-rubik md:text-[18px] lg:text-[12px] 2xl:text-[18px]",
+
   // glassmorphism
   glassEffect:
     " dark:bg-white bg-opacity-10 bg-black dark:bg-opacity-10 shadow-normal dark:shadow-5xl backdrop-filter backdrop-blur-sm ",
